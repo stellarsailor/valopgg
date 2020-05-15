12da630986ebd92bd674c99e71eeaa955efec739
stellarsailor <dev.stellarsailor@gmail.com>
2020-05-15 13:39:46 -0400

    beta word deleted for appstore

--- src/datas/guide.tsx
+++ src/datas/guide.tsx
@@ -25,12 +25,12 @@ export const guide: Array<groupType> = [
         tabs: [
             {
                 id: 0,
-                tabName: '클로즈베타 0.50',
+                tabName: 'v0.50',
                 markdown: patchMD[0],
             },
             {
                 id: 1,
-                tabName: '클로즈베타 0.49',
+                tabName: 'v0.49',
                 markdown: patchMD[1],
             }
         ]
