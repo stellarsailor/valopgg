dcb3c84cfb3ea7cb074790866ffad5322518090a
stellarsailor <dev.stellarsailor@gmail.com>
2020-06-01 05:50:30 -0400

    Adfit need to disable when dev

--- src/components/subcomponents/Adfit.js
+++ src/components/subcomponents/Adfit.js
@@ -1,34 +1,3 @@
-// import React, { useEffect } from 'react';
-
-
-// export function Adfit() { 
-    
-//     // useEffect(() => {
-//     //     window.ba = window.ba || []
-//     //     window.ba.min.push({});
-//     // },[])
-
-//     useEffect(() => {
-//         console.log('asddassa')
-//     })
-
-//     if(window.innerWidth < 576){
-//         return (<></>)
-//     } else {
-//         return (
-//             <div key={Math.random()}>
-//                 <ins className="kakao_ad_area" 
-//                 key={Math.random()}
-//                 style={{display: 'none'}}
-//                 data-ad-unit    = "DAN-qe74ylevqulf" 
-//                 data-ad-width   = "728" 
-//                 data-ad-height  = "90"></ins> 
-//             </div>
-//         )
-//     }
-// }
-
-
 import React from 'react';
 import loadScript from '../../logics/load-script';
 
@@ -39,26 +8,30 @@ export default class Adfit extends React.Component {
     }
 
     componentWillMount() {
-        window.adfit || loadScript('https://t1.daumcdn.net/kas/static/ba.min.js', function(){})
-        console.log('loadingScript')
-
+        // window.adfit || loadScript('https://t1.daumcdn.net/kas/static/ba.min.js', function(){})
+        loadScript('https://t1.daumcdn.net/kas/static/ba.min.js', function(){})
+        // console.log('loadingScript')
     }
 
     componentDidMount() {
-        // console.log(document.getElementById('daumAd'))
-        // window.adfit.render()
-        console.log('rendered')
+        // console.log(document.getElementById('daumAd')) 원래 주석
+        // window.adfit.render() 원래 주석
+
+        // console.log('rendered')
     }
 
     componentWillUnmount() {
+        // console.log(window.adfit)
         window.adfit.destroy(this.props.adUnit)
-        console.log('destroy')
+
+        // console.log('destroy')
     }
 
     render() {
+
         return (
-            <div key={Math.random()}>
-                <ins className={'kakao_ad_area'}
+            <div key={Math.random} style={ this.props.margin ? {display: 'flex', justifyContent: 'center', margin: '1rem'} : {display: 'flex', justifyContent: 'center'}}>
+                {/* <ins className={'kakao_ad_area'}
                     style={{
                         display: 'block',
                         width: '100%'
@@ -66,11 +39,11 @@ export default class Adfit extends React.Component {
                     data-ad-unit={this.props.adUnit}
                     data-ad-width={this.props.adWidth}
                     data-ad-height={this.props.adHeight}
-                    data-ad-test={this.props.adTest || 'N'}
+                    data-ad-test={true} //this.props.adTest || 'N'}
                     data-ad-param-channel={this.props.adParamChannel}
                     data-ad-param-cp={this.props.adParamCp}
                     >
-                </ins>
+                </ins> */}
             </div>
             )
     }
