0829c6d5121eceddbff731015a64874ec1fa3ed6
stellarsailor <dev.stellarsailor@gmail.com>
2020-06-17 15:55:02 -0400

    weapon rotation backend, category korean

--- src/components/Weapon.tsx
+++ src/components/Weapon.tsx
@@ -1,13 +1,14 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import MetaTags from 'react-meta-tags';
 import styled from 'styled-components'
-import { Row, Col, Carousel, BackTop } from 'antd';
+import { Row, Col, Carousel, BackTop, Spin } from 'antd';
 import { weapon } from '../datas/weapon'
 import { skinCollection } from '../datas/weaponSkin'
 import WeaponRender from './subcomponents/WeaponRender';
 import Adfit from './subcomponents/Adfit';
-import { staticServer } from '../serverUrl'
+import { staticServer, apiServer } from '../serverUrl'
 import Countdown from 'react-countdown';
+import Axios from 'axios';
 
 const WeaponCategory = styled.div`
     background-color: #273552;
@@ -19,6 +20,21 @@ const WeaponCategory = styled.div`
 `
 
 export default function Weapon(){
+
+    const [ rotation, setRotation ] = useState({
+        date: '',
+        collection: '',
+    })
+    const [ asyncLoading, setAsyncLoading ] = useState(true)
+
+    useEffect(() => {
+        Axios.get(`${apiServer}/weapon/rotation`)
+        .then(res => {
+            const data = res.data;
+            setRotation(data)
+            setAsyncLoading(false)
+        })
+    },[])
 
     return (
         <Row justify="center" style={{backgroundColor: 'rgba(19, 28, 46, 0.95)'}} >
@@ -38,33 +54,41 @@ export default function Weapon(){
                     }
                 </Carousel> */}
                 {/* { window.innerWidth < 576 ? <Adfit adUnit="DAN-skxug7w2pkjd" adWidth="320" adHeight="100" /> : <Adfit adUnit="DAN-qe74ylevqulf" adWidth="728" adHeight="90" /> } */}
-            <Countdown date={'2020-06-18T06:00:00.000+09:00'} renderer={({ days, hours, minutes, seconds}) => <span style={{fontWeight: 'bold', position: 'absolute', bottom: 10, left: 10, fontSize: '1.6rem'}}>{days}:{hours}:{minutes}:{seconds}</span>} />
-                <img src={`${staticServer}/weaponcollections/sovereign.png`} style={{width: '100%', alignSelf: 'flex-start',}} />
+            {asyncLoading ? 
+                <div style={{width: '100%', height: 100, display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                    <Spin />
+                </div>
+            :
+                <div>
+                    <Countdown date={rotation.date} renderer={({ days, hours, minutes, seconds}) => <span style={{fontWeight: 'bold', position: 'absolute', bottom: 10, left: 10, fontSize: '1.6rem'}}>{days}:{hours}:{minutes}:{seconds}</span>} />
+                    <img src={`${staticServer}/weaponcollections/${rotation.collection}.png`} style={{width: '100%', alignSelf: 'flex-start',}} />
+                </div>
+            }
             </Col>
             <Col xs={24} sm={22} md={20} lg={20} xl={15} style={{marginBottom: '1rem'}}>
                 <Row style={{width: '100%', height: '100%', backgroundColor: '#202b43'}} justify="center">
                     <Col xs={24} sm={12} md={12} lg={4} xl={4} style={{ padding: 10, display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
-                        <WeaponCategory>Sidearm</WeaponCategory>
+                        <WeaponCategory>보조 무기</WeaponCategory>
                         {weapon.filter((v) => v.category === 'Sidearm').map((v, index) => <WeaponRender key={index} name={v.name} name_kr={v.name_ko} cost={v.cost} resize={true} />)}
                     </Col>
                     <Col xs={24} sm={12} md={12} lg={6} xl={6} style={{ padding: 10, display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
-                        <WeaponCategory>SMG</WeaponCategory>
+                        <WeaponCategory>기관단총</WeaponCategory>
                         {weapon.filter((v) => v.category === 'SMG').map((v, index) => <WeaponRender key={index} name={v.name} name_kr={v.name_ko} cost={v.cost} />)}
-                        <WeaponCategory>Shotgun</WeaponCategory>
+                        <WeaponCategory>산탄총</WeaponCategory>
                         {weapon.filter((v) => v.category === 'Shotgun').map((v, index) => <WeaponRender key={index} name={v.name} name_kr={v.name_ko} cost={v.cost} />)}
                     </Col>
                     <Col xs={24} sm={12} md={12} lg={7} xl={7} style={{ padding: 10, display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
-                        <WeaponCategory>Rifle</WeaponCategory>
+                        <WeaponCategory>소총</WeaponCategory>
                         {weapon.filter((v) => v.category === 'Rifle').map((v, index) => <WeaponRender key={index} name={v.name} name_kr={v.name_ko} cost={v.cost} />)}
                     </Col>
                     <Col xs={24} sm={12} md={12} lg={7} xl={7} style={{ padding: 10, display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
-                        <WeaponCategory>Sniper</WeaponCategory>
+                        <WeaponCategory>저격소총</WeaponCategory>
                         {weapon.filter((v) => v.category === 'Sniper').map((v, index) => <WeaponRender key={index} name={v.name} name_kr={v.name_ko} cost={v.cost} />)}
-                        <WeaponCategory>Heavy</WeaponCategory>
+                        <WeaponCategory>중형</WeaponCategory>
                         {weapon.filter((v) => v.category === 'Heavy').map((v, index) => <WeaponRender key={index} name={v.name} name_kr={v.name_ko} cost={v.cost} />)}
                     </Col>
                     <Col xs={24} sm={12} md={12} lg={7} xl={7} style={{ padding: 10, display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
-                        <WeaponCategory>knife</WeaponCategory>
+                        <WeaponCategory>근접 무기</WeaponCategory>
                         <WeaponRender key="10" name="Knife" name_kr="전술용 나이프" cost="0" />
                     </Col>
                 </Row>
