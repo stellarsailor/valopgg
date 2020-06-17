238fa06093f5ca787c00c2cb82244fb87a8428b1
stellarsailor <dev.stellarsailor@gmail.com>
2020-06-16 22:46:00 -0400

    map image changed and barrier added

--- src/components/MapDetail.tsx
+++ src/components/MapDetail.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import MetaTags from 'react-meta-tags';
 import styled from 'styled-components'
 import { Link } from "react-router-dom";
-import { Row, Col, Radio } from 'antd';
+import { Row, Col, Radio, Checkbox } from 'antd';
 import { PlusOutlined, MinusOutlined } from '@ant-design/icons';
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 import Adfit from './subcomponents/Adfit';
@@ -36,6 +36,7 @@ export default function MapDetail(props) {
 
     const mapName = props.match.params.name;
     const [ side, setSide ] = useState("attacker")
+    const [ barrier, setBarrier ] = useState(true)
     
     return (
         <Row justify="center" style={{backgroundColor: 'rgba(19, 28, 46, 1)', minHeight: 800}} >
@@ -46,7 +47,7 @@ export default function MapDetail(props) {
                 </MetaTags>
                 <Row>
                     {mapArr.map((v,index) => (
-                        <Col xs={12} sm={12} md={12} lg={12} xl={12} style={{width: '100%', padding: 10}} key={index}>
+                        <Col xs={12} sm={12} md={6} lg={6} xl={6} style={{width: '100%', padding: 10}} key={index}>
                             <Link to={`/map/${v.toLowerCase()}`}>
                                 <div style={ mapName === v.toLowerCase() ? {backgroundImage: `url(${Images[index]})`, width: "100%", height: '100%', backgroundSize: 'cover', backgroundPosition: 'center', borderRadius: 5, border: '1px solid lightgray'} : {backgroundImage: `url(${Images[index]})`, width: "100%", height: '100%', backgroundSize: 'cover', backgroundPosition: 'center', borderRadius: 5, border: '1px solid rgba(19, 28, 46, 1)', filter: 'grayscale(100%)'} }>
                                     <BottomText>{v}</BottomText>
@@ -62,11 +63,14 @@ export default function MapDetail(props) {
                 </div> */}
                 {/* {window.innerWidth < 576 ? <Adfit adUnit="DAN-skxug7w2pkjd" adWidth="320" adHeight="100" /> : <Adfit adUnit="DAN-qe74ylevqulf" adWidth="728" adHeight="90" /> } */}
 
-                <div style={{display: 'flex', justifyContent: 'center', margin: '10px 0'}}>
+                <div style={{display: 'flex', justifyContent: 'center', margin: '10px 0', position: 'absolute', right: 0, zIndex: 20}}>
                     <Radio.Group onChange={(e) => setSide(e.target.value)} value={side} >
                         <Radio value={"attacker"} style={{color: 'white'}}>공격 진영</Radio>
                         <Radio value={"defender"} style={{color: 'white'}}>수비 진영</Radio>
                     </Radio.Group>
+                    <Checkbox checked={barrier} onChange={(e) => setBarrier(e.target.checked)} style={{color: 'white'}}>
+                        배리어
+                    </Checkbox>
                 </div>
                 {
                     window.innerWidth < 576 ?
@@ -80,6 +84,11 @@ export default function MapDetail(props) {
                                     <TransformComponent >
                                         <div style={{width: '100%', height: '100%'}}>
                                             <img src={`${staticServer}/map/${mapName}-labels-${side}-ko.png`} style={{width: '100%', position: 'absolute', zIndex: 10}} alt="발로란트 맵 용어 콜아웃" />
+                                            {
+                                                barrier ? 
+                                                <img src={`${staticServer}/map/${mapName}-layout-barrier.png`} style={side === "attacker" ? {width: '100%', position: 'absolute', zIndex: 5} : {width: '100%', position: 'absolute', zIndex: 5, transform: 'rotate(180deg)'}} alt={`Valorant map ${mapName}`} />
+                                                : null
+                                            }
                                             <img src={`${staticServer}/map/${mapName}-layout-base.png`} style={side === "attacker" ? {width: '100%'} : {width: '100%', transform: 'rotate(180deg)'}} alt={`Valorant map ${mapName}`} />
                                         </div>
                                     </TransformComponent>
@@ -89,11 +98,16 @@ export default function MapDetail(props) {
                     :
                     <div>
                         <img src={`${staticServer}/map/${mapName}-labels-${side}-ko.png`} style={{width: '100%', position: 'absolute', zIndex: 10}} alt="발로란트 맵 용어 콜아웃" />
+                        {
+                            barrier ? 
+                            <img src={`${staticServer}/map/${mapName}-layout-barrier.png`} style={side === "attacker" ? {width: '100%', position: 'absolute', zIndex: 5} : {width: '100%', position: 'absolute', zIndex: 5, transform: 'rotate(180deg)'}}  alt={`Valorant map ${mapName}`} />
+                            : null
+                        }
                         <img src={`${staticServer}/map/${mapName}-layout-base.png`} style={side === "attacker" ? {width: '100%'} : {width: '100%', transform: 'rotate(180deg)'}}  alt={`Valorant map ${mapName}`} />
                     </div>
                 }
                 <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
-                    <div style={{fontWeight: 'bold', fontSize: '1.6rem'}}>{capitalizeFirstLetter(mapName)}</div>
+                    <div style={{fontWeight: 'bold', fontSize: '1.6rem', fontStyle: 'italic'}}>{capitalizeFirstLetter(mapName)}</div>
                     <div style={{margin: '2rem 0', fontSize: '1.4rem'}}>
                         해당 콜아웃(용어)은 발로그에서 임의로 붙인 용어이며, 발로란트의 공식 용어가 아닙니다.
                     </div>
