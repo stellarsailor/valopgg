3c99175e8fe32d24eec13d78ef77ed1e058884d8
stellarsailor <dev.stellarsailor@gmail.com>
2020-08-04 18:20:33 -0400

    battlepass act 2, killjoy update

--- src/components/subcomponents/CalculatorBattlepass.tsx
+++ src/components/subcomponents/CalculatorBattlepass.tsx
@@ -16,9 +16,25 @@ const BigText = styled.div`
     margin-bottom: 1rem;
 `
 
+const TabMenu = styled.span`
+    font-size: ${props => props.isMobile ? '1.2rem' : '1.4rem' };
+    border-bottom: ${props => props.selected ? '1px solid white' : '0px solid gray' };
+    color: ${props => props.selected ? 'white' : 'gray' };
+`
+
 export default function CalculatorBattlepass () {
 
-    const battlepassWithExp = battlepass.map(v => {
+    const [ selectedAct, setSelectedAct ] = useState<number>(battlepass.length - 1);
+    const [ openList, setOpenList ] = useState(false)
+
+    const handleAct = useCallback((act: number) => {
+        setSelectedAct(act)
+        setOpenList(false)
+        setStartTier(0)
+        setGoalTier(0)
+    },[])
+
+    const battlepassWithExp = battlepass[selectedAct].contents.map(v => {
         const expObj = { exp : (v.tier * 1000) + 2000 }
         return { ...v, ...expObj}
     })
@@ -36,85 +52,105 @@ export default function CalculatorBattlepass () {
 
     useEffect(() => {
         if(startTier !== 0 && goalTier !== 0){
-            let sumTemp = 0
-            battlepassWithExp.map(v => {
-                if(startTier < v.tier && v.tier <= goalTier)
-                sumTemp += v.exp
-            })
-            setSumExp(sumTemp)
+            if(startTier == 50 && goalTier == 50){
+                setSumExp(53000)
+            } else {
+                let sumTemp = 0
+                battlepassWithExp.map(v => {
+                    if(startTier < v.tier && v.tier <= goalTier)
+                    sumTemp += v.exp
+                })
+                setSumExp(sumTemp)
+            }
         }
     },[startTier, goalTier])
 
     return (
         <Row>
-            <Col xs={24} sm={24} md={24} lg={24} xl={24}>
-                <div style={{fontSize: '1.4rem', margin: '1rem'}}>
-                    이그니션 : 제 1막(~ 8/5 06:00)
-                </div>
-            </Col>
-            <Col xs={24} sm={24} md={24} lg={12} xl={12} style={{backgroundColor: 'rgb(24, 35, 56)', border: '0px solid gray', width: '100%'}}>
-                <div style={{height: 400, overflowY: 'scroll', padding: 10}}>
-                    {battlepassWithExp.map(v => 
-                    <>
-                        <RenderEachTier selected={ startTier <= v.tier && v.tier <= goalTier ? true : false }>
-                            <div style={startTier <= v.tier && v.tier <= goalTier ? {color: 'black'} : {color: 'white'}}>
-                                Tier {v.tier} - {v.reward}
-                            </div> 
-                            <div>
-                                <a onClick={() => setStartTier(v.tier)} style={startTier <= v.tier && v.tier <= goalTier ? {color: 'black'} : {color: 'white'}}> 
-                                    [From] 
-                                </a> 
-                                <a onClick={() => setGoalTier(v.tier)} style={startTier <= v.tier && v.tier <= goalTier ? {color: 'black'} : {color: 'white'}}> 
-                                    [To] 
-                                </a>
-                            </div>
-                        </RenderEachTier>
+            {
+                openList ? 
+                <Col xs={24} sm={24} md={24} lg={12} xl={12} style={{}}>
+                    {battlepass.map( (v, index) => 
+                        <div style={{margin: '1rem 0'}} >
+                            <a onClick={() => handleAct(index)}>
+                                <TabMenu selected={true} >{v.title}</TabMenu>
+                            </a>
+                        </div>
+                        )}
+                </Col>
+                :
+                <>
+                <Col xs={24} sm={24} md={24} lg={24} xl={24}>
+                    <div style={{fontSize: '1.4rem', margin: '1rem'}}>
+                        {battlepass[selectedAct].title} <span style={{fontSize: '1.2rem'}}>({battlepass[selectedAct].deadline})</span> 
+                        <a style={{color: 'gray', marginLeft: '1rem', fontSize: '1.2rem'}} onClick={() => setOpenList(true)}>목록</a>
+                    </div>
+                </Col>
+                <Col xs={24} sm={24} md={24} lg={12} xl={12} style={{backgroundColor: 'rgb(24, 35, 56)', border: '0px solid gray', width: '100%'}}>
+                    <div style={{height: 400, overflowY: 'scroll', padding: 10}}>
+                        {battlepassWithExp.map(v => 
+                        <>
+                            <RenderEachTier selected={ startTier <= v.tier && v.tier <= goalTier ? true : false }>
+                                <div style={startTier <= v.tier && v.tier <= goalTier ? {color: 'black'} : {color: 'white'}}>
+                                    Tier {v.tier} - {v.reward}
+                                </div> 
+                                <div>
+                                    <a onClick={() => setStartTier(v.tier)} style={startTier <= v.tier && v.tier <= goalTier ? {color: 'black'} : {color: 'white'}}> 
+                                        [From] 
+                                    </a> 
+                                    <a onClick={() => setGoalTier(v.tier)} style={startTier <= v.tier && v.tier <= goalTier ? {color: 'black'} : {color: 'white'}}> 
+                                        [To] 
+                                    </a>
+                                </div>
+                            </RenderEachTier>
+                            {
+                                v.tier % 5 === 0 ?
+                                <div style={{borderBottom: '1px solid lightgray', margin: '1rem 0'}}>
+                                </div>
+                                :
+                                null
+                            }
+                            </>
+                            )}
+                    </div>
+                </Col>
+                <Col xs={24} sm={24} md={24} lg={12} xl={12} style={{backgroundColor: '#273552', border: '0px solid gray', width: '100%', padding: '1rem'}}>
+                    <div style={{fontSize: '1.4rem', borderBottom: '1px solid lightgray', paddingBottom: '1rem' , marginBottom: '1rem'}}>
+                        필요 경험치량 : <span style={{fontWeight: 'bold'}}>{sumExp}</span>
+                    </div>
+                    <Checkbox onChange={e => handlePcBangScailing(e.target.checked)} style={{color: 'white', textAlign: 'right', marginBottom: '1rem'}}>
+                        피시방 120% 경험치 적용
+                    </Checkbox>
+                    <BigText>
+                        대략 필요한 일반 게임 판수 : {Math.ceil(sumExp / (3800 * pcBangScailing))} 판
+                    </BigText>
+                    <BigText>
+                        소요 시간(평균 35분) : { Math.floor(Math.ceil(sumExp / (3800 * pcBangScailing)) * 35 / 60) } 시간 { Math.ceil(sumExp / (3800 * pcBangScailing)) * 35 % 60 } 분
+                    </BigText>
+                    <BigText>
+                        스파이크 돌격 게임 판수 : {Math.ceil(sumExp / (1000 * pcBangScailing))} 판
+                    </BigText>
+                    <BigText>
+                        소요 시간(평균 10분) : { Math.floor(Math.ceil(sumExp / (1000 * pcBangScailing)) * 10 / 60) } 시간 { Math.ceil(sumExp / (1000 * pcBangScailing)) * 10 % 60 } 분
+                    </BigText>
+                    
+                    <BigText>
+                        현질로 뚫는다면?
                         {
-                            v.tier % 5 === 0 ?
-                            <div style={{borderBottom: '1px solid lightgray', margin: '1rem 0'}}>
+                        goalTier - startTier > 0 ?
+                            <div>
+                                { (goalTier - startTier) * 300 } VP = 약 {(goalTier - startTier) * 300 * 11} 원
                             </div>
-                            :
+                        :
                             null
                         }
-                        </>
-                        )}
-                </div>
-            </Col>
-            <Col xs={24} sm={24} md={24} lg={12} xl={12} style={{backgroundColor: '#273552', border: '0px solid gray', width: '100%', padding: '1rem'}}>
-                <div style={{fontSize: '1.4rem', borderBottom: '1px solid lightgray', paddingBottom: '1rem' , marginBottom: '1rem'}}>
-                    필요 경험치량 : <span style={{fontWeight: 'bold'}}>{sumExp}</span>
-                </div>
-                <Checkbox onChange={e => handlePcBangScailing(e.target.checked)} style={{color: 'white', textAlign: 'right', marginBottom: '1rem'}}>
-                    피시방 120% 경험치 적용
-                </Checkbox>
-                <BigText>
-                    대략 필요한 일반 게임 판수 : {Math.ceil(sumExp / (3800 * pcBangScailing))} 판
-                </BigText>
-                <BigText>
-                    소요 시간(평균 35분) : { Math.floor(Math.ceil(sumExp / (3800 * pcBangScailing)) * 35 / 60) } 시간 { Math.ceil(sumExp / (3800 * pcBangScailing)) * 35 % 60 } 분
-                </BigText>
-                <BigText>
-                    스파이크 돌격 게임 판수 : {Math.ceil(sumExp / (1000 * pcBangScailing))} 판
-                </BigText>
-                <BigText>
-                    소요 시간(평균 10분) : { Math.floor(Math.ceil(sumExp / (1000 * pcBangScailing)) * 10 / 60) } 시간 { Math.ceil(sumExp / (1000 * pcBangScailing)) * 10 % 60 } 분
-                </BigText>
-                
-                <BigText>
-                    현질로 뚫는다면?
-                    {
-                    goalTier - startTier > 0 ?
-                        <div>
-                            { (goalTier - startTier) * 300 } VP = 약 {(goalTier - startTier) * 300 * 11} 원
-                        </div>
-                    :
-                        null
-                    }
-                </BigText>
-                <div>
-                    해당 판수와 소요 시간은 주간/일일 퀘스트를 제외한 수치이므로 일부 차이가 있을 수 있습니다.
-                </div>
-            </Col>
+                    </BigText>
+                    <div>
+                        해당 판수와 소요 시간은 주간/일일 퀘스트를 제외한 수치이므로 일부 차이가 있을 수 있습니다.
+                    </div>
+                </Col>
+                </>
+            }
         </Row>
     )
 }
--- src/datas/battlepass.tsx
+++ src/datas/battlepass.tsx
@@ -1,206 +1,418 @@
-export const battlepass = [
-{
-    tier: 1,
-    reward: '킹덤 버키',
-},
-{ 
-    tier: 2,
-    reward: '행운을 빈다냥 총기 장식',
-},
-{ 
-    tier: 3,
-    reward: '10 레디어나이트 포인트',
-},
-{ 
-    tier: 4,
-    reward: '어센트 카드',
-},
-{ 
-    tier: 5,
-    reward: '킹덤 스펙터',
-},
-{ 
-    tier: 6,
-    reward: '폭탄 코기 스프레이',
-},
-{ 
-    tier: 7,
-    reward: '상자 총기 장식',
-},
-{ 
-    tier: 8,
-    reward: '10 레디어나이트 포인트',
-},
-{ 
-    tier: 9,
-    reward: '레이나 ID 카드',
-},
-{ 
-    tier: 10,
-    reward: '쿠튀르 불독',
-},
-{ 
-    tier: 11,
-    reward: '클래식 도면 카드',
-},
-{ 
-    tier: 12,
-    reward: '위험 스프레이',
-},
-{ 
-    tier: 13,
-    reward: '10 레디어나이트 포인트',
-},
-{ 
-    tier: 14,
-    reward: '치즈 총기장식',
-},
-{ 
-    tier: 15,
-    reward: '쿠튀르 스팅어',
-},
-{ 
-    tier: 16,
-    reward: '쿠튀르 프렌지',
-},
-{ 
-    tier: 17,
-    reward: '캠퍼 스프레이',
-},
-{ 
-    tier: 18,
-    reward: '고대의 신비 카드',
-},
-{ 
-    tier: 19,
-    reward: '10 레디어나이트 포인트',
-},
-{ 
-    tier: 20,
-    reward: '쿠튀르 마샬',
-},
-{ 
-    tier: 21,
-    reward: '저기요? 스프레이',
-},
-{ 
-    tier: 22,
-    reward: '10 레디어나이트 포인트',
-},
-{ 
-    tier: 23,
-    reward: '도넛 총기 장식',
-},
-{ 
-    tier: 24,
-    reward: '정전 스프레이',
-},
-{ 
-    tier: 25,
-    reward: '킹덤 팬텀',
-},
-{ 
-    tier: 26,
-    reward: '비열한 공격 스프레이',
-},
-{ 
-    tier: 27,
-    reward: '10 레디어나이트 포인트',
-},
-{ 
-    tier: 28,
-    reward: 'GG 슈터 스프레이',
-},
-{ 
-    tier: 29,
-    reward: '스펙터 도면 카드',
-},
-{ 
-    tier: 30,
-    reward: 'DOT EXE 오딘',
-},
-{ 
-    tier: 31,
-    reward: '발로란트 마스코트 카드',
-},
-{ 
-    tier: 32,
-    reward: '10 레디어나이트 포인트',
-},
-{ 
-    tier: 33,
-    reward: '피자 총기 장식',
-},
-{ 
-    tier: 34,
-    reward: '승리를 위해 스프레이',
-},
-{ 
-    tier: 35,
-    reward: 'DOT EXE 고스트',
-},
-{ 
-    tier: 36,
-    reward: '파괴의 삶 스프레이',
-},
-{ 
-    tier: 37,
-    reward: '총알 총기 장식',
-},
-{ 
-    tier: 38,
-    reward: '발로란트 새벽 카드',
-},
-{ 
-    tier: 39,
-    reward: '10 레디어나이트 포인트',
-},
-{ 
-    tier: 40,
-    reward: 'DOT EXE 저지',
-},
-{ 
-    tier: 41,
-    reward: '10 레디어나이트 포인트',
-},
-{ 
-    tier: 42,
-    reward: '오퍼레이터 도면 카드',
-},
-{ 
-    tier: 43,
-    reward: '헤드샷 스프레이',
-},
-{ 
-    tier: 44,
-    reward: '레디어나이트 상자 총기 장식',
-},
-{ 
-    tier: 45,
-    reward: 'DOT EXE 밴달',
-},
-{ 
-    tier: 46,
-    reward: '주먹 인사 스프레이',
-},
-{ 
-    tier: 47,
-    reward: '떠오른 어센트 총기 장식',
-},
-{ 
-    tier: 48,
-    reward: '기밀 카드',
-},
-{ 
-    tier: 49,
-    reward: '10 레디어나이트 포인트',
-},
-{ 
-    tier: 50,
-    reward: '킹덤 근접 무기',
-},
+export const battlepass = 
+[
+    {
+        title: "이그니션 : 액트 1",
+        deadline: '~ 8/5',
+        contents: [
+            {
+                tier: 1,
+                reward: '킹덤 버키',
+            },
+            { 
+                tier: 2,
+                reward: '행운을 빈다냥 총기 장식',
+            },
+            { 
+                tier: 3,
+                reward: '10 레디어나이트 포인트',
+            },
+            { 
+                tier: 4,
+                reward: '어센트 카드',
+            },
+            { 
+                tier: 5,
+                reward: '킹덤 스펙터',
+            },
+            { 
+                tier: 6,
+                reward: '폭탄 코기 스프레이',
+            },
+            { 
+                tier: 7,
+                reward: '상자 총기 장식',
+            },
+            { 
+                tier: 8,
+                reward: '10 레디어나이트 포인트',
+            },
+            { 
+                tier: 9,
+                reward: '레이나 ID 카드',
+            },
+            { 
+                tier: 10,
+                reward: '쿠튀르 불독',
+            },
+            { 
+                tier: 11,
+                reward: '클래식 도면 카드',
+            },
+            { 
+                tier: 12,
+                reward: '위험 스프레이',
+            },
+            { 
+                tier: 13,
+                reward: '10 레디어나이트 포인트',
+            },
+            { 
+                tier: 14,
+                reward: '치즈 총기장식',
+            },
+            { 
+                tier: 15,
+                reward: '쿠튀르 스팅어',
+            },
+            { 
+                tier: 16,
+                reward: '쿠튀르 프렌지',
+            },
+            { 
+                tier: 17,
+                reward: '캠퍼 스프레이',
+            },
+            { 
+                tier: 18,
+                reward: '고대의 신비 카드',
+            },
+            { 
+                tier: 19,
+                reward: '10 레디어나이트 포인트',
+            },
+            { 
+                tier: 20,
+                reward: '쿠튀르 마샬',
+            },
+            { 
+                tier: 21,
+                reward: '저기요? 스프레이',
+            },
+            { 
+                tier: 22,
+                reward: '10 레디어나이트 포인트',
+            },
+            { 
+                tier: 23,
+                reward: '도넛 총기 장식',
+            },
+            { 
+                tier: 24,
+                reward: '정전 스프레이',
+            },
+            { 
+                tier: 25,
+                reward: '킹덤 팬텀',
+            },
+            { 
+                tier: 26,
+                reward: '비열한 공격 스프레이',
+            },
+            { 
+                tier: 27,
+                reward: '10 레디어나이트 포인트',
+            },
+            { 
+                tier: 28,
+                reward: 'GG 슈터 스프레이',
+            },
+            { 
+                tier: 29,
+                reward: '스펙터 도면 카드',
+            },
+            { 
+                tier: 30,
+                reward: 'DOT EXE 오딘',
+            },
+            { 
+                tier: 31,
+                reward: '발로란트 마스코트 카드',
+            },
+            { 
+                tier: 32,
+                reward: '10 레디어나이트 포인트',
+            },
+            { 
+                tier: 33,
+                reward: '피자 총기 장식',
+            },
+            { 
+                tier: 34,
+                reward: '승리를 위해 스프레이',
+            },
+            { 
+                tier: 35,
+                reward: 'DOT EXE 고스트',
+            },
+            { 
+                tier: 36,
+                reward: '파괴의 삶 스프레이',
+            },
+            { 
+                tier: 37,
+                reward: '총알 총기 장식',
+            },
+            { 
+                tier: 38,
+                reward: '발로란트 새벽 카드',
+            },
+            { 
+                tier: 39,
+                reward: '10 레디어나이트 포인트',
+            },
+            { 
+                tier: 40,
+                reward: 'DOT EXE 저지',
+            },
+            { 
+                tier: 41,
+                reward: '10 레디어나이트 포인트',
+            },
+            { 
+                tier: 42,
+                reward: '오퍼레이터 도면 카드',
+            },
+            { 
+                tier: 43,
+                reward: '헤드샷 스프레이',
+            },
+            { 
+                tier: 44,
+                reward: '레디어나이트 상자 총기 장식',
+            },
+            { 
+                tier: 45,
+                reward: 'DOT EXE 밴달',
+            },
+            { 
+                tier: 46,
+                reward: '주먹 인사 스프레이',
+            },
+            { 
+                tier: 47,
+                reward: '떠오른 어센트 총기 장식',
+            },
+            { 
+                tier: 48,
+                reward: '기밀 카드',
+            },
+            { 
+                tier: 49,
+                reward: '10 레디어나이트 포인트',
+            },
+            { 
+                tier: 50,
+                reward: '킹덤 근접 무기',
+            },
+        ]
+    },
+    {
+        title: "이그니션 : 액트 2",
+        deadline: '~ 10/14',
+        contents: [
+            {
+                tier: 1,
+                reward: '하이브마인드 아레스',
+            },
+            { 
+                tier: 2,
+                reward: '분열된 스플릿 총기 장식',
+            },
+            { 
+                tier: 3,
+                reward: '10 레디어나이트 포인트',
+            },
+            { 
+                tier: 4,
+                reward: '저지 도면 카드',
+            },
+            { 
+                tier: 5,
+                reward: '하이브마인드 스펙터',
+            },
+            { 
+                tier: 6,
+                reward: '독성 없음 스프레이',
+            },
+            { 
+                tier: 7,
+                reward: '위험한 도마뱀 총기 장식',
+            },
+            { 
+                tier: 8,
+                reward: '10 레디어나이트 포인트',
+            },
+            { 
+                tier: 9,
+                reward: '어림없는 소리 카드',
+            },
+            { 
+                tier: 10,
+                reward: '폴리폭스 저지',
+            },
+            { 
+                tier: 11,
+                reward: '폴리폭스 카드',
+            },
+            { 
+                tier: 12,
+                reward: '댑 팀 스프레이',
+            },
+            { 
+                tier: 13,
+                reward: '10 레디어나이트 포인트',
+            },
+            { 
+                tier: 14,
+                reward: '폴리폭스 총기장식',
+            },
+            { 
+                tier: 15,
+                reward: '폴리폭스 불독',
+            },
+            { 
+                tier: 16,
+                reward: '폴리폭스 셰리프',
+            },
+            { 
+                tier: 17,
+                reward: '킬조이 ID 카드',
+            },
+            { 
+                tier: 18,
+                reward: '양파 꼬마 스프레이',
+            },
+            { 
+                tier: 19,
+                reward: '10 레디어나이트 포인트',
+            },
+            { 
+                tier: 20,
+                reward: '폴리폭스 가디언',
+            },
+            { 
+                tier: 21,
+                reward: '핵 사용 금지 스프레이',
+            },
+            { 
+                tier: 22,
+                reward: '10 레디어나이트 포인트',
+            },
+            { 
+                tier: 23,
+                reward: '개구리 모자 총기 장식',
+            },
+            { 
+                tier: 24,
+                reward: '역대 최고 스프레이',
+            },
+            { 
+                tier: 25,
+                reward: '하이브마인드 밴달',
+            },
+            { 
+                tier: 26,
+                reward: '슬픈 게 스프레이',
+            },
+            { 
+                tier: 27,
+                reward: '10 레디어나이트 포인트',
+            },
+            { 
+                tier: 28,
+                reward: '전략가 토끼 스프레이',
+            },
+            { 
+                tier: 29,
+                reward: '하이브마인드 카드',
+            },
+            { 
+                tier: 30,
+                reward: '적색경보 버키',
+            },
+            { 
+                tier: 31,
+                reward: '결정적인 순간 카드',
+            },
+            { 
+                tier: 32,
+                reward: '10 레디어나이트 포인트',
+            },
+            { 
+                tier: 33,
+                reward: '적색경보 총기 장식',
+            },
+            { 
+                tier: 34,
+                reward: '시간이 없어 스프레이',
+            },
+            { 
+                tier: 35,
+                reward: '적색경보 스팅어',
+            },
+            { 
+                tier: 36,
+                reward: '화면 포착 스프레이',
+            },
+            { 
+                tier: 37,
+                reward: '안전한 헤이븐 총기 장식',
+            },
+            { 
+                tier: 38,
+                reward: 'VS // 피닉스 + 제트 카드',
+            },
+            { 
+                tier: 39,
+                reward: '10 레디어나이트 포인트',
+            },
+            { 
+                tier: 40,
+                reward: '적색경보 클래식',
+            },
+            { 
+                tier: 41,
+                reward: '10 레디어나이트 포인트',
+            },
+            { 
+                tier: 42,
+                reward: '오딘 도면 카드',
+            },
+            { 
+                tier: 43,
+                reward: '이것도 괜찮네 스프레이',
+            },
+            { 
+                tier: 44,
+                reward: '왕관 컵 총기 장식',
+            },
+            { 
+                tier: 45,
+                reward: '적색경보 오퍼레이터',
+            },
+            { 
+                tier: 46,
+                reward: '한계를 돌파하라 스프레이',
+            },
+            { 
+                tier: 47,
+                reward: '턴테이블 총기 장식',
+            },
+            { 
+                tier: 48,
+                reward: '승리로 가는 길 카드',
+            },
+            { 
+                tier: 49,
+                reward: '10 레디어나이트 포인트',
+            },
+            { 
+                tier: 50,
+                reward: '하이브마인드 검',
+            },
+        ]
+    }
 ]
-
 // 4000
 // 5000
 // 6000
